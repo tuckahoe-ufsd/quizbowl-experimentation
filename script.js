96c6aedfaea401955fc19eb911d82feaaf8782b1
Elizabeth <elizabethg2796@tuckahoeschools.org>
2024-11-13 13:02:43 +0000

    logging into edge

--- script.js
+++ script.js
@@ -3,12 +3,19 @@ const  document.getElementById('message').innerText = displayMessage;
 
 let isCorrect = true;
 
-
 button.addEventListener('click', () => {
     if (isCorrect) {
          displayMessage = "Correct";
+         console.log("meeeeeh");
     } else {
         displayMessage = "Wrong, Try Again";
+        console.log("meeeeeh");
 }
     isCorrect = !isCorrect;
 });
+
+let answerList = ['1', '2', '3']
+const question = document.querySelector('.shell')
+question.textContent = answerList[0]
+
+let score = 0; 
