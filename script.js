e6f9afc16662fdee030e4fbe7cc4f871109136f2
Elizabeth <elizabethg2796@tuckahoeschools.org>
2024-11-14 13:32:45 +0000

    adding genuine javascript

--- script.js
+++ script.js
@@ -1,10 +1,26 @@
+let questionList = ['Who was the 16th President of the United States of America?', 'Who won the WDC in F1 in 2016?', 'What is my second favorite NHL team?', 'What is my favorite F1 team?', 'Who was President during Watergate?', 'Whose my favorite hockey player? (not including patty kane!)', 'What is my favorite TV show?', "What is my least favorite hockey team?", 'Who are my three favorite F1 drivers all time? (Last names only, alphabetical order)', 'How do you spell my last name?']; 
+let answerList = ['Abraham Lincoln', 'Nico Rosberg', 'Detriot Redwings', 'Red Bull', 'Richard Nixon', 'Connor McDavid', 'Daredevil', 'New York Islanders', 'Rosberg, Verstappen, Vettel', 'Gjelaj']; 
 let score = 0; 
-let isCorrect = true;
-let answerList = ['1', '2', '3'];
-const button = document.querySelector('#button'); 
-const question = document.querySelector('.questions');
-//const question = document.querySelector('.shell');
+let questionIndex = 0; 
 
-button.addEventListener('click', () => {
-    console.log("yipee");
-});
+const question = document.querySelector('.questioncon'); 
+const button = document.querySelector('.buttoncon'); 
+const input = document.querySelector('#input-container'); 
+let scoreContainer = document.querySelector('.scorecon');
+
+question.textContent = questionList[questionIndex];
+
+let sumbitFunction = () =>{
+    button.addEventListener('click', () =>{
+        if(input.value === answerList[questionIndex]){
+            questionIndex += 1; 
+            score += 1; 
+            question.textContent = questionList[questionIndex]; 
+            scoreContainer.textContent = "Score: " + score; 
+        }else{
+            score 
+        }
+    });
+}
+
+//10:19
